ced00ee158d91d2ba10333ea0b4879dc1b98aaeb
ellisrourke <ellisrourke@gmail.com>
2021-05-17 09:34:35 +1000

    bacon

--- Q4/bacon.cpp
+++ Q4/bacon.cpp
@@ -1,6 +1,3 @@
-//
-// Created by Ellis on 10/05/2021.
-//
 #include <unordered_map>
 #include <string>
 #include <vector>
@@ -10,6 +7,8 @@
 #include <sstream>
 #include <fstream>
 #include <chrono>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 class Actor{
@@ -33,16 +32,15 @@ public:
 
 class Graph{
 public:
-    int count;
+    int count=0;
     unordered_map<string, Actor *> graph;
 
     Graph () : graph(){}
     Actor *retrieveNode(string name){
         if(graph.find(name) == graph.end()){
             return NULL;
-        } else {
-            return graph[name];
         }
+        return graph[name];
     }
 
     void addNode(string actor, string movie){
@@ -62,6 +60,7 @@ public:
         string movieName;
         string actorName;
         string line;
+        int count =0;
         cout << "[?] Attempting to open file: " << filename << endl;
 
         file.open(filename);
@@ -73,18 +72,19 @@ public:
             cout << "[+] Reading data from file " << endl;
         }
 
-        while(getline(file,line)){
-            istringstream token(line);
-            getline(token, actorName, '|');
-            getline(token, movieName, '|');
-            addNode(actorName, movieName);
-            //cout << "The actor " << actorName << " was in the movie " << movieName << endl;
+        if(file.is_open()){
+            while(getline(file,line)){
+                istringstream token(line);
+                getline(token, actorName, '|');
+                getline(token, movieName, '|');
+                addNode(actorName, movieName);
+                count++;
+                //cout << "The actor " << actorName << " was in the movie " << movieName << endl;
+            }
         }
         file.close();
         cout << "[+] Successfully read data from file " << endl;
-        file.clear();
-        file.seekg(0);
-
+        cout << "[+] Read " << count << " lines from file " << endl;
     }
 
     void displayPath(Actor *target){
@@ -98,56 +98,74 @@ public:
         cout << "Bacon Number: " << count / 2 << endl;
     }
 
-    Actor *breadthFirstSearch(Actor *start, Actor *target, bool findMax){
-        if(start == NULL or target == NULL){
-            cout << "[-] Unable to find key" << endl;
-        }
+    void populate(){
+        ifstream myReadFile;
+        string output, name, movie;
+        myReadFile.open("../data/bacon1.txt");
 
-        queue<Actor *>q;
-        set<Actor *> s;
-        Actor *last;
-
-        q.push(start);
-        s.insert(start);
-        while(!q.empty()){
-            Actor *current = q.front();
-            last = current->isActor ? current : NULL;
-            q.pop();
-
-            if(current->name == target->name && !findMax){
-                return current;
-            }
-
-
-            for(int i=0; i<current->connections.size(); i++){
-                if(s.count(current->connections[i]) == 0){
-                    s.insert(current->connections[i]);
-                    q.push(current->connections[i]);
-                    current->connections[i]->parentActor = current;
-                }
+        if(myReadFile.is_open()){
+            while(getline(myReadFile, output)){
+                istringstream token(output);
+                getline(token, name, '|');
+                getline(token, movie, '|');
+                addNode(name, movie);
             }
         }
-    return last;
+        myReadFile.close();
     }
+
+
 };
 
-int main(int argc, char *argv[]){
-    unordered_map<string, vector<string>> movie_actor;
-    unordered_map<string, vector<string>> actor_movie;
-    unordered_map<string, Actor> actor;
-
-    if(argc < 4){
-        cout << "Incorrect Arguments Passed" << endl;
-        exit(1);
+Actor *breadthFirstSearch(Actor *start, Actor *target, bool findMax){
+    if(start == NULL or target == NULL){
+        cout << "[-] Unable to find key" << endl;
+        return 0;
     }
 
+    queue<Actor *>q;
+    set<Actor *> s;
+    Actor *last;
+
+    q.push(start);
+    s.insert(start);
+    while(!q.empty()){
+        Actor *current = q.front();
+        last = current->isActor ? current : NULL;
+        q.pop();
+
+        if(current->name == target->name && !findMax){
+            return current;
+        }
+
+
+        for(int i=0; i<current->connections.size(); i++){
+            if(s.count(current->connections[i]) == 0){
+                s.insert(current->connections[i]);
+                q.push(current->connections[i]);
+                current->connections[i]->parentActor = current;
+            }
+        }
+    }
+    return last;
+}
+
+int main(int argc, char *argv[]){
+    //if(argc < 4){
+   //     cout << "Incorrect Arguments Passed" << endl;
+    //    exit(1);
+   // }
+
     string filename=argv[1];
-    string startActor = argv[2];
-    string targetActor = argv[3];
+    //string startActor = argv[2];
+    //string targetActor = argv[3];
+    string startActor = "Kevin Bacon (I)";
+    string targetActor = "Denzel Washington";
+
     cout << "[?] Start Actor: " << startActor << endl;
     cout << "[?] Target Actor: " << targetActor << endl << endl;
 
-
+/*
     Graph imdb_a;
     imdb_a.readFile((string)filename);
     Actor *start = imdb_a.retrieveNode(startActor);
@@ -167,16 +185,26 @@ int main(int argc, char *argv[]){
     Actor *end = imdb_b.breadthFirstSearch(startb, targetb, true);
     imdb_b.displayPath(end);
     cout << " --------- " << endl;
-
+*/
 /////////////////////////////////////////////////////////////////
 
     Graph imdb_c;
     cout << "- Part C -" << endl;
-    imdb_c.readFile(string(filename));
-    Actor *start_c = imdb_c.retrieveNode("Gerald W. Abrams");
-    Actor *end_c = imdb_c.retrieveNode("Don Ackerman (III)");
-    imdb_c.breadthFirstSearch(start_c, end_c, false);
-    imdb_c.displayPath(end_c);
+    imdb_c.readFile((string)filename);
+    Actor *start_c = imdb_c.retrieveNode("Christian Slate");
+    Actor *end_c = imdb_c.retrieveNode("Catherine Zeta-Jones");
+
+    for (auto const& name : imdb_c.graph)
+    {
+        std::cout << name.first << endl;
+    }
+
+
+    cout << "start " << start_c->name << endl;
+    cout << "end " << end_c->name << endl;
+
+    breadthFirstSearch(start_c, end_c, false);
+    imdb_c.displayPath(start_c);
     cout << " --------- " << endl;
 
 
